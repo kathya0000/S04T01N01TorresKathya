0b3518058c6c9f68202f416fb8e6a13aa3e4823a
Sprint1_IT <kathya19@hotmail.com>
2023-09-10 19:03:21 +0200

    Initial commit

---
+++ S04T01N01TorresKathya/src/main/java/cat/itacademy/barcelonactiva/torres/kathya/s04/t01/n01/S04T01N01TorresKathya/S04T01N01TorresKathyaApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.torres.kathya.s04.t01.n01.S04T01N01TorresKathya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01TorresKathyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01TorresKathyaApplication.class, args);
+	}
+
+}
---
+++ S04T01N01TorresKathya/src/test/java/cat/itacademy/barcelonactiva/torres/kathya/s04/t01/n01/S04T01N01TorresKathya/S04T01N01TorresKathyaApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.torres.kathya.s04.t01.n01.S04T01N01TorresKathya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N01TorresKathyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
